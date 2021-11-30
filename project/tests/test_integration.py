348e38a3ceb049fbb2d3d58ee1c0cddb4590b2f5
Igor Mihailov <dr.migo@ya.ru>
2021-11-30 20:18:43 +0300

    Integration tests finished

--- project/tests/test_integration.py
+++ project/tests/test_integration.py
@@ -8,6 +8,7 @@ from werkzeug.security import generate_password_hash
 from ..models import User
 from .. import db
 from .. import auth
+from .. import prediction
 
 class TestInt(TestCase):
 
@@ -50,12 +51,21 @@ class TestInt(TestCase):
 
     # Each test method starts with the keyword test_
 
-    def test_save_data(self):
+    def test_save_and_predict_success(self):
 
-        # auth
+        # auth data
         email = 'new_user@gmail.com'
         name = 'admin'
         password = '123456'
+
+        # profile data
+        phone = '777-333'
+        passport = '8614152326'
+        age = 10
+        income = 150
+        emp_length = 0
+        defaults_in_past = 'Y'
+        hist_length = 0
 
         new_user = User(email=email,
                         name=name,
@@ -75,7 +85,13 @@ class TestInt(TestCase):
         user_data = dict(
             email = email,
             name = name,
-            passport='8614152326')
+            phone = phone,
+            passport = passport,
+            age = age,
+            income = income,
+            emp_length = emp_length,
+            defaults_in_past = defaults_in_past,
+            hist_length = hist_length)
 
         response = self.profile_save_request(user_data)
 
@@ -83,21 +99,140 @@ class TestInt(TestCase):
                 flash_message = dict(session['_flashes']).get('message')
 
         cur_user = db.session.query(User).filter(User.email == email).first()
+
+        # current template
         self.assert_template_used("profile.html")
+
+        # message on page
         self.assertIsNotNone(flash_message, session['_flashes'])
         self.assertMessageFlashed("Saved successfully!", 'message')
 
-        self.assertIsNotNone(cur_user.passport, cur_user)
-        #self.assertIn(b'Passport must bе 10 numbers!', response.data)
+        # check data in db
+        self.assertEqual(cur_user.phone, phone)
+        self.assertEqual(cur_user.passport, passport)
+        self.assertEqual(cur_user.age, age)
+        self.assertEqual(cur_user.income, income)
+        self.assertEqual(cur_user.emp_length, emp_length)
+        self.assertEqual(cur_user.defaults_in_past, defaults_in_past)
+        self.assertEqual(cur_user.hist_length, hist_length)
+
+        # predict for loan
+        data_str = {"person_age":cur_user.age,
+                    "person_income":cur_user.income,
+                    "person_emp_length":cur_user.emp_length,
+                    "loan_amnt":6475000,
+                    "loan_int_rate":9.999,
+                    "loan_percent_income":0.9,
+                    "cb_person_cred_hist_length":cur_user.hist_length,
+                    "person_home_ownership": "RENT",
+                    "loan_intent": "MEDICAL",
+                    "loan_grade": "E",
+                    "cb_person_default_on_file": cur_user.defaults_in_past}
+
+        prediction_result = prediction.predict(data_str)
+
+        self.assertEqual(prediction_result, 1)
+
+    def test_save_data_passport_expired(self):
+
+        # auth data
+        email = 'new_user@gmail.com'
+        name = 'admin'
+        password = '123456'
+
+        # profile data
+        passport = '8614152327' # in expired list
+
+        new_user = User(email=email,
+                        name=name,
+                        password=generate_password_hash(password, method='sha256'))
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # login
+        response = self.login_request(email, password)
+
+        self.assertIsNotNone(current_user, "User didn't login!")
+        self.assert_template_used("profile.html")
+
+        # save data
+        user_data = dict(
+            email = email,
+            name = name,
+            passport = passport)
+
+        response = self.profile_save_request(user_data)
+
+        with self.client.session_transaction() as session:
+                flash_message = dict(session['_flashes']).get('error')
+
+        cur_user = db.session.query(User).filter(User.email == email).first()
+
+        self.assert_template_used("profile.html")
+
+        # message on page
+        self.assertIsNotNone(flash_message, session['_flashes'])
+        self.assertMessageFlashed("Passport in expired list!", 'error')
+
+        # passport didn't write to db
+        self.assertIsNone(cur_user.passport, cur_user)
+
+    def test_save_data_passport_wrong(self):
+
+        # auth data
+        email = 'new_user@gmail.com'
+        name = 'admin'
+        password = '123456'
+
+        # profile data
+        passport = '12345' # in expired list
+
+        new_user = User(email=email,
+                        name=name,
+                        password=generate_password_hash(password, method='sha256'))
+
+        # add the new user to the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # login
+        response = self.login_request(email, password)
+
+        self.assertIsNotNone(current_user, "User didn't login!")
+        self.assert_template_used("profile.html")
+
+        # save data
+        user_data = dict(
+            email = email,
+            name = name,
+            passport = passport)
+
+        response = self.profile_save_request(user_data)
+
+        with self.client.session_transaction() as session:
+                flash_message = dict(session['_flashes']).get('error')
+
+        cur_user = db.session.query(User).filter(User.email == email).first()
+
+        self.assert_template_used("profile.html")
+
+        # message on page
+        self.assertIsNotNone(flash_message, session['_flashes'])
+        self.assertMessageFlashed("Passport must bе 10 numbers!", 'error')
+
+        # passport didn't write to db
+        self.assertIsNone(cur_user.passport, cur_user)
 
     def login_request(self, email, password):
         return self.client.post('/login', data=dict(
-            email=email,
+                email=email,
             password=password
         ), follow_redirects=True)
 
     def profile_save_request(self, user_data):
-        return self.client.post('/profile', data=user_data, follow_redirects = True)
+        return self.client.post('/profile', data = user_data, follow_redirects = True)
 
 # Executing the tests in the above test case class
 if __name__ == "__main__":
